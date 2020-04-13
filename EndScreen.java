c10b115805056dcc0ad8e5cf80fc0bf83870a9c1
AnthonyDuan <62152480+AnthonyDuan@users.noreply.github.com>
2020-04-13 05:02:14 -0400

    Add files via upload

--- EndScreen.java
+++ EndScreen.java
@@ -6,17 +6,13 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 public class EndScreen extends JPanel implements ActionListener{
-	//issues:
-		/*
-		 * i really just need to test the stuff
-		 * and of course the rotating text
-		 */
+	
 	private double score; private JButton replay; private JButton Score; private JButton replay1; private JButton Score1; private Game game;
 	public void paintComponent (Graphics g) {
 		super.paintComponent(g);
 		BufferedImage i;
 		try {
-			i = ImageIO.read(new File("src/MazeBack.jpg"));
+			i = ImageIO.read(new File("src/GoodBackgroundAD.jpg"));
 
 		}
 		catch (Exception e) {
@@ -31,20 +27,20 @@ public class EndScreen extends JPanel implements ActionListener{
 		g.setFont(new Font ("Arial", Font.PLAIN, (int)(height/20))); 
 		for (int count = 0; count < 5; count++) {
 			int first = count+1; int second = count+6;
-			g.drawString(first+": "+minList.get(first-1), (int)(width*.13), (int)(height*.55+height*.06*count)); //figure out where to store values
-			g.drawString(second+": "+minList.get(second-1), (int)(width*.71), (int)(height*.55+height*.06*count));
+			g.drawString(first+": "+minList.get(first-1), (int)(width*.13), (int)(height*.5+height*.075*count)); //figure out where to store values
+			g.drawString(second+": "+minList.get(second-1), (int)(width*.73)-((int)(second/10)*17), (int)(height*.5+height*.075*count));
 		}
 		g.setFont(new Font ("Arial", Font.BOLD, (int)(height/8)));
 		g.setColor(new Color (96,96,96));
-		g.drawString("You Escaped!", (int)(width*.25)-5, (int)(height*.33)-2);
+		g.drawString("You Escaped!", (int)(width*.19)-5, (int)(height*.29)-2);
 		g.setColor(Color.WHITE);
-		g.drawString("You Escaped!", (int)(width*.25), (int)(height*.33));
+		g.drawString("You Escaped!", (int)(width*.19), (int)(height*.29));
 		g.setColor(new Color (96,96,96));
-		g.fillRoundRect((int) (width*.385)-5, (int)(height*.72)+7, (int) (width*.23), (int) (height*.17), (int) (width*.015), (int) (width*.015));
-		g.fillRoundRect((int) (width*.385)-5, (int)(height*.49)+7, (int) (width*.23), (int) (height*.17), (int) (width*.015), (int) (width*.015));
+		g.fillRoundRect((int) (width*.385)-5, (int)(height*.66)+7, (int) (width*.23), (int) (height*.17), (int) (width*.015), (int) (width*.015));
+		g.fillRoundRect((int) (width*.385)-5, (int)(height*.43)+7, (int) (width*.23), (int) (height*.17), (int) (width*.015), (int) (width*.015));
 		g.setColor(new Color (178,0,0));
-		g.fillRoundRect((int) (width*.385), (int)(height*.49), (int) (width*.23), (int) (height*.17), (int) (width*.015), (int) (width*.015));
-		g.fillRoundRect((int) (width*.385), (int)(height*.72), (int) (width*.23), (int) (height*.17), (int) (width*.015), (int) (width*.015));
+		g.fillRoundRect((int) (width*.385), (int)(height*.43), (int) (width*.23), (int) (height*.17), (int) (width*.015), (int) (width*.015));
+		g.fillRoundRect((int) (width*.385), (int)(height*.66), (int) (width*.23), (int) (height*.17), (int) (width*.015), (int) (width*.015));
 	} 
 	private ArrayList <Double> getMinimumScores (ArrayList<Double> list) {
 		int keeping = -1; 
@@ -82,18 +78,18 @@ public class EndScreen extends JPanel implements ActionListener{
 		this.game = game;
 		int width = 800;
 		int height = 600;
-		score = (double) (int)(score*1000)/1000;
+		score = (double) Math.round((score*1000))/1000;
 		this.score=score;
 		replay = new JButton ("Replay"); 
 		replay.setBackground(new Color (178,0,0)); replay.setForeground(Color.WHITE);
 		replay.setFont(new Font ("Arial", Font.PLAIN, (int) (height*.13*.35)));
 		replay.setSize((int) (width*.21), (int) (height*.15));
-		replay.setLocation((int) (width*.395), (int)(height*.73));
+		replay.setLocation((int) (width*.395), (int)(height*.67));
 		this.add(replay);
 		Score = new JButton("Score: "+score);  Score.setBackground(new Color (178,0,0)); Score.setForeground(Color.WHITE);
-		Score.setFont(new Font ("Arial", Font.PLAIN, (int) (height*.13*.35)));
+		Score.setFont(new Font ("Arial", Font.PLAIN, (int) (height*.13*.3)));
 		Score.setSize((int) (width*.21), (int) (height*.15));
-		Score.setLocation((int) (width*.395), (int)(height*.5));
+		Score.setLocation((int) (width*.395), (int)(height*.44));
 		replay.addActionListener(this);
 		this.add(Score);
 	}
