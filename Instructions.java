c187e4207a033deda8294e57ff1695984389a88f
Darry Lu <36702191+ldu0893@users.noreply.github.com>
2020-04-24 02:17:26 -0400

    Update Instructions.java

--- Instructions.java
+++ Instructions.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-public class Instructions extends JPanel implements ActionListener{
+public class Instructions extends JPanel implements ActionListener, MouseListener{
 	private Game game; private JButton backButton;
 	public void paintComponent (Graphics g) {
 		super.paintComponent(g);
@@ -12,8 +12,12 @@ public class Instructions extends JPanel implements ActionListener{
 		int height = 600;
 		try {
 			final BufferedImage i = ImageIO.read(new File("OtherImages/ArrowThing.png"));
-			int axel=51;
-			g.drawImage(i,482,244,120,120,null); 
+			final BufferedImage j = ImageIO.read(new File("Icons/Menu Button.png"));
+			final BufferedImage k = ImageIO.read(new File("Icons/Map Button 1.png"));
+			//int axel=51;
+			g.drawImage(i, 482, 244, null); 
+			g.drawImage(j, 150, 390, null);
+			g.drawImage(k, 150, 470, null);
 		}
 		catch (Exception e) {
 		}
@@ -23,23 +27,28 @@ public class Instructions extends JPanel implements ActionListener{
 		g.setColor(Color.WHITE);
 		g.drawString("Instructions:", (int)(width*.05), (int)(height*.16));
 		g.setFont(new Font ("Arial", Font.PLAIN, (int)(height/20)));
-		g.drawString("TAB (toggle): Opens the map", (int)(width*.06), (int)(height*.34));
-		g.drawString("Left Arrow Key: Rotate left", (int)(width*.06), (int)(height*.405));
-		g.drawString("Right Arrow Key: Rotate right", (int)(width*.06), (int)(height*.47));
-		g.drawString("Up Arrow Key: Go up", (int)(width*.06), (int)(height*.6));
-		g.drawString("Down Arroy Key: Go Down", (int)(width*.06), (int)(height*.665));
-		g.drawString("Space Bar: Go forward", (int)(width*.06), (int)(height*.73));
+		g.setColor(Color.red);
+		g.drawString("The exit to the maze is at the", (int)(width*.06), (int)(height*.26));
+		g.drawString("bottom left corner!", (int)(width*.06), (int)(height*.33));
+		g.setColor(Color.white);
+		g.drawString("Use arrow keys to rotate", (int)(width*.06), (int)(height*.455));
+		g.drawString("and navigate, space bar", (int)(width*.06), (int)(height*.52));
+		g.drawString("to go forward.", (int)(width*.06), (int)(height*.59));
 		g.setFont(new Font ("Arial", Font.PLAIN, (int)(height/30)));
-		g.drawString("Left arrow: Rotate left", (int)(width*.58), (int)(height*.23));
-		g.drawString("Right arrow: Rotate right", (int)(width*.58), (int)(height*.27));
-		g.drawString("Middle arrow: Go forward", (int)(width*.58), (int)(height*.31));
-		g.drawString("Up arrow: Go up", (int)(width*.58), (int)(height*.35));
-		g.drawString("Down arrow: Go down", (int)(width*.58), (int)(height*.39));
-		g.drawString("Triple line: Expands menu", (int)(width*.58), (int)(height*.7));
-		g.drawString("Map button: Opens map", (int)(width*.58), (int)(height*.74));
+		g.setColor(Color.decode("#4dff4d"));
+		g.drawString("Rotate left", 395, 324);
+		g.drawString("Rotate right", 643, 324);
+		//g.drawString("Middle arrow: Go forward", (int)(width*.58), (int)(height*.31));
+		g.drawString("Go up", 535, 241);
+		g.drawString("Go down", 520, 414);
+		g.drawString("Middle arrow: Go forward", 450, 442);
 		g.setFont(new Font ("Arial", Font.BOLD, (int)(height/25)));
-		g.drawString("Click buttons on the D-pad:", (int)(width*.58), (int)(height*.18));
-		g.drawString("Menu buttons:", (int)(width*.58), (int)(height*.65));
+		g.setColor(Color.white);
+		g.drawString("Map:", 50, 505);
+		g.setColor(Color.blue);
+		g.drawString("D-pad Buttons:", 480, 200);
+		g.setColor(Color.white);
+		g.drawString("Menu:", 50, 425);
 		g.setColor(new Color (96,96,96));
 		g.fillRoundRect((int) (width*.45)-4, (int)(height*.79)+3, (int) (width*.36), (int) (height*.11), (int) (width*.015), (int) (width*.015));
 		g.setColor(new Color (178,0,0));
@@ -64,6 +73,33 @@ public class Instructions extends JPanel implements ActionListener{
 		backButton.setLocation((int) (width*.46), (int)(height*.79));
 		backButton.setFont(new Font ("Arial", Font.PLAIN, (int)(height/13)));
 		this.add(backButton);
+		addMouseListener(this);
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		int releasedX=e.getX();
+		int releasedY=e.getY();
+		System.out.println(releasedX+" "+releasedY);
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
